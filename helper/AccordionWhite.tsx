f746c92a681152988ffd4134f9ad33aefd7cd395
Baaska <faitynb@gmail.com>
2024-11-23 14:06:21 +0800

    faq page

--- helper/AccordionWhite.tsx
+++ helper/AccordionWhite.tsx
@@ -3,9 +3,9 @@ interface AccordionProps {
     content: string;
 }
 
-const Accordion: React.FC<AccordionProps> = ({ title, content }) => {
+const AccordionWhite: React.FC<AccordionProps> = ({ title, content }) => {
     return (
-        <details className="group bg-[#EBEFF8] px-5 rounded-xl">
+        <details className="group  pr-5  border-b border-[#D9D9D9]">
             <summary className="flex cursor-pointer list-none items-center gap-3 py-4 text-lg font-medium text-[#353644] group-open:text-primary-500">
                 <div className="text-[#3B77EA]">
                     <svg
@@ -29,15 +29,12 @@ const Accordion: React.FC<AccordionProps> = ({ title, content }) => {
                         stroke="currentColor"
                         className="hidden h-5 w-5 group-open:block"
                     >
-                        <path
-                            
-                            d="M19.5 12h-15"
-                        />
+                        <path d="M19.5 12h-15" />
                     </svg>
                 </div>
                 {title}
             </summary>
-            <div className="pb-4 text-[#353644] px-10 pt-5">
+            <div className="pb-4 text-[#353644] px-10  pt-5">
                 <div
                     dangerouslySetInnerHTML={{
                         __html: content,
@@ -48,4 +45,4 @@ const Accordion: React.FC<AccordionProps> = ({ title, content }) => {
     );
 };
 
-export default Accordion;
+export default AccordionWhite;
